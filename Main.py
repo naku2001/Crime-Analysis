68d56484c918975fc8b70f89e9c302b26b951cd2
Perfect-Princess Makuwerere <93464813+Perfect-Princess@users.noreply.github.com>
2023-05-30 12:35:07 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import pickle
 from pathlib import Path
 
 import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
+
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 
